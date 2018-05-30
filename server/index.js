718f695801a6fed7dbbd69642177a9162105bfb8
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-30 15:11:06 +1000

    form submit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // app.get('/', (req, res) => res.send('Bonjour Le Monde!'))
 app.use('/', express.static('../client'))
@@ -7,5 +10,10 @@ app.use('/', express.static('../client'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 app.post('/contact', (req, res) => {
-  res.send('Submitted!')
+  res.send(`Submitted! 
+  <br>
+  Name: ${req.body.name}<br>
+  Email: ${req.body.email}<br>
+  Email: ${req.body.email}<br>
+  Message: ${req.body.message}<br>`)
 })
