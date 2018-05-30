2854394b62a304bbfe12740f7174bcaa328c4697
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-30 14:49:16 +1000

    nodemon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Bonjour Le Monde!'))
+// app.get('/', (req, res) => res.send('Bonjour Le Monde!'))
+app.use('/', express.static('../client'))
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
