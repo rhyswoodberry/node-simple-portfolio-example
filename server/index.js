c106124f6c0ce3da7efe987df3e6e0d13d7ba33e
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-30 15:05:22 +1000

    body-parser

--- server/index.js
+++ server/index.js
@@ -4,5 +4,8 @@ const app = express()
 // app.get('/', (req, res) => res.send('Bonjour Le Monde!'))
 app.use('/', express.static('../client'))
 
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.post('/contact', (req, res) => {
+  res.send('Submitted!')
+})
